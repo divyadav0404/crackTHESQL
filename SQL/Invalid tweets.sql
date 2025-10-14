6a8f87d787fc16f8bcbc96f668e2d7bb098f26b6
Divyansh Yadav <divyanshyadav206@gmail.com>
2025-10-14 17:28:34 +0530

    Update Invalid tweets.sql

--- SQL/Invalid tweets.sql
+++ SQL/Invalid tweets.sql
@@ -16,3 +16,21 @@ insert into Tweets (tweet_id, content) values ('2', 'More than fifteen chars are
 select tweet_id
 from Tweets
 where LENGTH(content) > 15;
+/*
+Input: 
+Tweets table:
++----------+-----------------------------------+
+| tweet_id | content                           |
++----------+-----------------------------------+
+| 1        | Let us Code                       |
+| 2        | More than fifteen chars are here! |
++----------+-----------------------------------+
+Output: 
++----------+
+| tweet_id |
++----------+
+| 2        |
++----------+
+Explanation: 
+Tweet 1 has length = 11. It is a valid tweet.
+Tweet 2 has length = 33. It is an invalid tweet. */
