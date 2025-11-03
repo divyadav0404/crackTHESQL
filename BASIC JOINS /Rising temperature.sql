9843096d1a26d8575264f67eefd8920ec0f5a916
Divyansh Yadav <divyanshyadav206@gmail.com>
2025-11-03 18:13:12 +0530

    rising temp.sql
    used the DATEDIFF() func of sql

---
+++ BASIC JOINS /Rising temperature.sql
@@ -0,0 +1,62 @@
+-- sql schema
+Create table If Not Exists Weather (id int, recordDate date, temperature int)
+Truncate table Weather
+insert into Weather (id, recordDate, temperature) values ('1', '2015-01-01', '10')
+insert into Weather (id, recordDate, temperature) values ('2', '2015-01-02', '25')
+insert into Weather (id, recordDate, temperature) values ('3', '2015-01-03', '20')
+insert into Weather (id, recordDate, temperature) values ('4', '2015-01-04', '30')
+-- sql ques
+Table: Weather
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| recordDate    | date    |
+| temperature   | int     |
++---------------+---------+
+id is the column with unique values for this table.
+There are no different rows with the same recordDate.
+This table contains information about the temperature on a certain day.
+ 
+
+Write a solution to find all dates' id with higher temperatures compared to its previous dates (yesterday).
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Weather table:
++----+------------+-------------+
+| id | recordDate | temperature |
++----+------------+-------------+
+| 1  | 2015-01-01 | 10          |
+| 2  | 2015-01-02 | 25          |
+| 3  | 2015-01-03 | 20          |
+| 4  | 2015-01-04 | 30          |
++----+------------+-------------+
+Output: 
++----+
+| id |
++----+
+| 2  |
+| 4  |
++----+
+Explanation: 
+In 2015-01-02, the temperature was higher than the previous day (10 -> 25).
+In 2015-01-04, the temperature was higher than the previous day (20 -> 30).
+
+
+sql query
+
+  
+select w1.id 
+from Weather w1
+Join Weather w2
+on DATEDIFF(w1.recordDate ,w2.recordDate) = 1
+where w1.temperature > w2.temperature 
