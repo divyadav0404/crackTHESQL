58af29c455d9e59e240abb9c03cc568618f36d85
Divyansh Yadav <divyanshyadav206@gmail.com>
2026-05-01 12:44:18 +0530

    Create query for average salary calculation
    Add SQL query to calculate average salary across all companies.

---
+++ SQL/Average Salary.sql
@@ -0,0 +1,8 @@
+--Write a query to calculate the average salary across all companies combined. Rename the column as avg_salary.
+
+-- your code goes here
+select Avg(salary) as avg_salary
+from works
+
+--select avg(salary) as avg_salary
+--from works
