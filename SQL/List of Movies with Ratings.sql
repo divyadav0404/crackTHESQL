284ee01c63f32ddd2c4b0a956178265a33c9d863
Divyansh Yadav <divyanshyadav206@gmail.com>
2026-05-08 15:41:58 +0530

    easy solution , select and use between query

---
+++ SQL/List of Movies with Ratings.sql
@@ -0,0 +1,5 @@
+--sol of cc ques
+Write a query to select only the movie names where the ratings are greater than 7 but less than 9.
+
+-- your code goes here
+  select Movie_name from Cinema where Rating between 7.1 and 8.9
